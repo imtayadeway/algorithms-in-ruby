f3da6bc1bda6e0ff83411a0b2e96ca68ae3908d0
Tim Wade <tim@majority.co>
2015-02-16 11:20:37 -0800

    Refactor quicksort.

--- sorting/quicksort.rb
+++ sorting/quicksort.rb
@@ -1,21 +1,6 @@
 def quicksort(array)
   return array if array.size <= 1
-
-  pivot = array.sample
-  array.delete_at(array.index(pivot))
-  less = []
-  greater = []
-  array.each do |element|
-    if element <= pivot
-      less << element
-    else
-      greater << element
-    end
-  end
-
-  sorted_array = []
-  sorted_array << *quicksort(less)
-  sorted_array << pivot
-  sorted_array << *quicksort(greather)
-  sorted_array
+  pivot = array.delete_at(array.index(array.sample))
+  less, greater = array.partition { |element| element <= pivot }
+  [*quicksort(less), pivot, *quicksort(greater)]
 end
