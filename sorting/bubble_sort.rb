48c10be252a2ee47c446a48f439233d4236c8acc
Tim Wade <tim@majority.co>
2015-02-16 12:06:41 -0800

    Refactor bubble sort.

--- sorting/bubble_sort.rb
+++ sorting/bubble_sort.rb
@@ -1,18 +1,14 @@
 def bubble_sort(array)
   return array if array.size <= 1
+  swapped = false
 
-  loop do
-    swapped = false
-
-    0.upto(array.size - 2) do |i|
-      j = i + 1
-      next unless array[i] > array[j]
-      array[i], array[j] = array[j], array[i]
-      swapped = true
-    end
-
-    break unless swapped
+  0.upto(array.size - 2) do |i|
+    j = i + 1
+    next unless array[i] > array[j]
+    array[i], array[j] = array[j], array[i]
+    swapped = true
   end
 
-  array
+  return array unless swapped
+  bubble_sort(array)
 end
